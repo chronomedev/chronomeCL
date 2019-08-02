1deacb838e2a32a3eef936d6c2179fae4078d6d9
Hansrenee Willysandro <hansrenee@live.com>
2019-08-02 21:40:56 +0700

    Updated files

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -20,18 +20,17 @@ namespace ConsoleApp1
         [STAThread]
         static void Main(string[] args)
         {
-            CudafyModes.Target = eGPUType.OpenCL; // To use OpenCL, change this enum inisialisasi mode
-            CudafyModes.DeviceId = 0; //ambil indeks pertama idnya
+            CudafyModes.Target = eGPUType.OpenCL;
+            CudafyModes.DeviceId = 0; 
             CudafyTranslator.Language = CudafyModes.Target == eGPUType.OpenCL ? eLanguage.OpenCL : eLanguage.Cuda;
             try
             {
                 int deviceCount = CudafyHost.GetDeviceCount(CudafyModes.Target);
                 if (deviceCount == 0)
                 {
-                    Console.WriteLine("No suitable {0} devices found.", CudafyModes.Target);
-                    goto theEnd;
-                }
-                Console.WriteLine("--------------Program Processing VGA ------------");
+                    Console.WriteLine("Tidak ada device ditemukan {0}", CudafyModes.Target);
+                } else {
+Console.WriteLine("--------------Program Processing VGA ------------");
                 Console.WriteLine("-------------- C h r o n o M E Dev ------------");
                 GPGPU gpu = CudafyHost.GetDevice(CudafyModes.Target, CudafyModes.DeviceId); // ngambil device gpunya yang terdaftar pertama sesuai device id
                 Console.WriteLine("Radeon RX 570/580 Series" );
@@ -61,25 +60,6 @@ namespace ConsoleApp1
             {
                 Console.WriteLine(ex);
             }
-        theEnd:
-            Console.ReadKey();
         }
-
-        //public static void kirim_webclient()
-        //{
-        //    WebClient klien = new WebClient();
-        //    klien.Headers.Add("Content-Typeyd", "binary/octet-stream");
-        //    byte[] result = klien.UploadFile("http://unicraftstore.com/wp-includes/server_pemvis/kelola_file.php", "POST", @"C:\Users\Hansrenee\Pictures\yorha-type-b-hd-wallpapers-67675-3372375.jpg");
-
-        //    string s = System.Text.Encoding.UTF8.GetString(result, 0, result.Length);
-        //    Console.WriteLine("HASIL VALUE DARI SERVER: " + s);
-        //    Console.ReadKey();
-        //}
-        //static void Main(string[] args)
-        //{
-        //    kirim_webclient();
-
-        //}
-
     }
 }
